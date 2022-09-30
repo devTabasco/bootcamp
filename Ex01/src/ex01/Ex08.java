6bd371da17783fdb5c9a00b59199817841228666
Changyong(Jack) <98437328+devTabasco@users.noreply.github.com>
2022-09-30 16:30:31 +0900

    day1

---
+++ Ex01/src/ex01/Ex08.java
@@ -0,0 +1,20 @@
+package ex01;
+
+public class Ex08 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String name = "홍길동";
+		int age = 27;
+		float weight = 77.4f;
+		boolean isMarried = true;
+		
+		System.out.println("이름 : "+name+" 입니다.");
+		System.out.println("나이 : "+age+"세");
+		System.out.println("몸무게 : "+weight+"kg");
+		System.out.println("결혼 : "+isMarried);
+
+	}
+
+}
