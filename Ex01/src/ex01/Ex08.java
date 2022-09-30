c624cc5a6cb04a5b74f88ccf5ffe18e335f4d4fe
Changyong(Jack) <98437328+devTabasco@users.noreply.github.com>
2022-09-30 16:39:35 +0900

    day1

--- Ex01/src/ex01/Ex08.java
+++ Ex01/src/ex01/Ex08.java
@@ -1,19 +1,33 @@
 package ex01;
 
+import java.util.Scanner;
+
 public class Ex08 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
 		
-		String name = "홍길동";
-		int age = 27;
-		float weight = 77.4f;
-		boolean isMarried = true;
+		System.out.print("이름을 입력해주세요 : ");
+		String name = sc.nextLine();
 		
+		System.out.print("나이를 입력해주세요 : ");
+		int age = sc.nextInt();
+		
+		System.out.print("몸무게를 입력해주세요 : ");
+		float weight = sc.nextFloat();
+		
+		System.out.print("결혼여부를 입력해주세요 : ");
+		boolean isMarried = sc.nextBoolean();
+		
+		System.out.println();
+		System.out.println("당신의 정보는 다음과 같습니다.");
 		System.out.println("이름 : "+name+" 입니다.");
 		System.out.println("나이 : "+age+"세");
 		System.out.println("몸무게 : "+weight+"kg");
 		System.out.println("결혼 : "+isMarried);
+		
+		sc.close();
 
 	}
 
